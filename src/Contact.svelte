7c910c619132ecc9c67dabf62f7d357cb61c2bd3
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2023-04-14 16:01:06 +0700

    Update Contact.svelte

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -10,7 +10,7 @@
             <img src="https://1.bp.blogspot.com/-3uU4-F2UHC0/YXBuFlUWq-I/AAAAAAAARaA/Ykp39LQzUDg1yUOIb_bsBD9jHYJFOQxXwCLcBGAsYHQ/s1920/16.jpg" alt="toko online whatsapp">
           </figure>
 		  <p>Dan hubungi kami jika butuh bantuan informasi maupun konsultasi</p>
-		  <a href="https://wa.me/6285646104747" class="button is-primary">Whatsapp</a> <a href="tel:+62895339403223" class="button is-primary">Call Us</a> <a href="mailto:axcora@gmail.com" class="button is-primary">Email</a> 
+		  <a href="https://wa.me/62895339403223" class="button is-primary">Whatsapp</a> <a href="tel:+62895339403223" class="button is-primary">Call Us</a> <a href="mailto:axcora@gmail.com" class="button is-primary">Email</a> 
 		</div>
   </div>
 <Footer/>
